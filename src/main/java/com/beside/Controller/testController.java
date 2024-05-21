7ed33f4b9b7c0e8795fdd2d7e37e84a30aa4648a
JayPark12 <106582851+JayPark12@users.noreply.github.com>
2024-05-22 00:24:47 +0900

    commit

--- src/main/java/com/beside/Controller/testController.java
+++ src/main/java/com/beside/Controller/testController.java
@@ -4,21 +4,23 @@ import lombok.extern.slf4j.Slf4j;
 import com.beside.service.testJava;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 @Controller
-@RequestMapping("/Test01")
+@RequestMapping("/kakao-login")
 public class testController {
     @Autowired
     private testJava testJava;
 
-    @PostMapping("/testRegister")
-    public  void testRErere(@RequestBody testEntity testEntity) {
+    @GetMapping("/")
+    public  String testRErere(@RequestBody testEntity testEntity) {
 
-        this.testJava.serviceTest(testEntity);
-
+        testEntity.setTestData1("123123");
         System.out.println(testEntity.getTestData1() + "완료");
+
+        return "index";
     }
 }
