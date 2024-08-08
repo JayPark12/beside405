d67ca3b2117d13daea3e42d48b4d02286bb04cc6
JayPark12 <106582851+JayPark12@users.noreply.github.com>
2024-08-09 01:31:23 +0900

    등산로 좌표 경위도로변경

--- src/main/java/com/beside/util/Coordinate.java
+++ src/main/java/com/beside/util/Coordinate.java
@@ -1,7 +1,11 @@
 package com.beside.util;
 
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.locationtech.proj4j.*;
 
 @Getter
 @Setter
@@ -12,9 +16,28 @@ public class Coordinate {
     private double y;
 
     public Coordinate(double[] coordinates) {
+
         if (coordinates != null && coordinates.length == 2) {
-            this.x = coordinates[0];
-            this.y = coordinates[1];
+            double utmX = coordinates[0];
+            double utmY = coordinates[1];
+
+            // 대한민국 좌표정보 중부기준
+            String ktmZone = "EPSG:5186";
+
+            // 변환기 설정
+            CRSFactory factory = new CRSFactory();
+            CoordinateReferenceSystem ktmCrs = factory.createFromName(ktmZone);
+            CoordinateReferenceSystem wgs84Crs = factory.createFromName("EPSG:4326");
+
+            CoordinateTransformFactory ctFactory = new CoordinateTransformFactory();
+            CoordinateTransform transform = ctFactory.createTransform(ktmCrs, wgs84Crs);
+
+            ProjCoordinate ktm = new ProjCoordinate(utmX, utmY);
+            ProjCoordinate latlon = new ProjCoordinate();
+            transform.transform(ktm, latlon);
+            //System.out.println("Latitude: " + latlon.y + ", Longitude: " + latlon.x);
+            this.y = latlon.y;
+            this.x = latlon.x;
         }
     }
 }
